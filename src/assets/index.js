5040cd2c748e165f1864bfddb32622a6a251bb2e
kenrich06 <kenrichsocials@gmail.com>
2025-03-22 16:45:40 +0530

    Initial commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./logo.svg"; // Updated to your custom logo
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -13,28 +13,9 @@ import css from "./tech/css.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import java from "./tech/java.png";
-import mysql from "./tech/mysql.png";
-import reactjs from "./tech/reactjs.png";
-import nextJs from "./tech/nextjs.svg";
-import angular from "./tech/angular.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import crmnext from "./company/crmnext.png";
-import nickelfox from "./company/nickelfox.png";
-import newgen from "./company/newgen.png";
-import skillrisers from "./company/skillrisers.png";
-import proximus from "./company/proximus.png";
+import java from "./tech/java.png"; // Last technology to keep
 
-import aptihealthWeb from "./projects/aptihealthWeb.png";
-import aptihealthApp from "./projects/aptihealthApp.png";
-import zealWeb from "./projects/zealWeb.png";
-import zealApp from "./projects/zealApp.png";
-import rollWeb from "./projects/rollWeb.png";
-import rollApp from "./projects/rollApp.png";
+import upcoming from "./projects/upcoming.png"; // Added upcoming.png
 
 export {
   logo,
@@ -51,25 +32,6 @@ export {
   git,
   html,
   javascript,
-  mongodb,
-  nodejs,
   java,
-  mysql,
-  reactjs,
-  nextJs,
-  angular,
-  redux,
-  tailwind,
-  typescript,
-  crmnext,
-  nickelfox,
-  newgen,
-  skillrisers,
-  proximus,
-  aptihealthWeb,
-  aptihealthApp,
-  zealWeb,
-  zealApp,
-  rollWeb,
-  rollApp,
+  upcoming, // Added upcoming.png
 };
